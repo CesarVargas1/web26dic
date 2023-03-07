50fa68535e92b1f5dd0e64a7f1f12c21de1dc893
CesarVargas1 <cavargas769@misena.edu.co>
2023-03-07 17:01:17 -0500

    nuevas imagenes menu tuberia

--- js/main.js
+++ js/main.js
@@ -149,8 +149,6 @@
         }
       })
     });
-
-
     
     /**
      * Scrool with ofset on links with a class name .scrollto
@@ -271,26 +269,6 @@
             clickable: true
         }
     });
-
-
-
-  /**
-   * Scrool with ofset on links with a class name .scrollto prueba
-   */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
-
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
-      scrollto(this.hash)
-    }
-  }, true)
 
   /**
    * Scroll with ofset on page load with hash links in the url
